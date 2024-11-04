7fcda5d5ab37b54dbbb05834df053c66bf0f1931
Mokete <emtyabekana@gmail.com>
2024-11-04 20:44:48 +0200

    Added an event listener to the form, and created a function to add a new job application by getting the user input with element id's.

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+// Accessing the form
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const jobForm=document.getElementById('jobForm');
+const applicationsTable=document.getElementById('applicationsTable').getElementsByTagName('bbody')[0];
+
+jobForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    addJobApplication();
+    jobForm.reset();
+});
+
+function addJobApplication(){
+
+  const company =document.getElementById('company');
+  const title =document.getElementById('title');
+  const date =document.getElementById('date');
+  const status =document.getElementById('status');
+
+}
+
+
+
+});
